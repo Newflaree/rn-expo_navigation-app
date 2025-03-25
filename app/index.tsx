2c61167cc38311be6015f4e9d862b4daa4947c4f
Camilo López <camiloj.lopez79@gmail.com>
2025-03-25 02:54:44 -0300

    Colores personalizados

--- app/index.tsx
+++ app/index.tsx
@@ -11,10 +11,11 @@ const App = () => {
       <View className='mt-6 mx-2.5'>
         <Text className='text-5xl' style={{ fontFamily: 'WorkSans-Black' }}>Navigation App</Text>
 
-        <Text className='text-4xl font-work-black'>Navigation App</Text>
+        <Text className='text-4xl font-work-black text-primary'>Navigation App</Text>
 
-        <Text className='text-3xl font-work-medium'>Navigation App</Text>
-        <Text className='text-2xl font-work-light'>Navigation App</Text>
+        <Text className='text-3xl font-work-medium text-secondary-200'>Navigation App</Text>
+        <Text className='text-2xl font-work-light text-secondary'>Navigation App</Text>
+        <Text className='text-xl font-work-light text-tertiary'>Navigation App</Text>
       </View>
     </SafeAreaView>
   );
