d4f9733ebee34dfd52a48f724101af198330d155
Camilo López <camiloj.lopez79@gmail.com>
2025-05-19 14:05:29 -0400

    Inicio de aplicación - NavigationApp

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+// React Native
+import { 
+  View,
+  Text
+} from 'react-native';
+
+
+const HomePage = () => {
+  return (
+    <View>
+      <Text>HomePage</Text>
+    </View>
+  );
+}
+
+export default HomePage;
