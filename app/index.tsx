a10b506dc03926459f85520aa82b79972c20d980
Camilo López <camiloj.lopez79@gmail.com>
2025-05-19 23:48:26 -0400

    Fuentes personalizadas en Nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
 // React Native
 import { 
+  SafeAreaView,
+  Text,
   View,
-  Text
 } from 'react-native';
 
 
 const HomePage = () => {
   return (
-    <View className='mt-10'>
-      <Text className='text-3xl font-bold text-blue-500'>HomePage</Text>
-    </View>
+    <SafeAreaView>
+      <View className='mt-6 mx-2.5'>
+        <Text className='text-5xl' style={{ fontFamily: 'WorkSans-Black' }}>HomePage</Text>
+
+        <Text className='text-4xl font-work-black'>HomePage</Text>
+        <Text className='text-3xl font-work-medium'>HomePage</Text>
+        <Text className='text-2xl font-work-light'>HomePage</Text>
+        <Text className='text-xl'>HomePage</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
