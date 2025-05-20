b5deed8d0cbe16c983439ca3f6dc862adffc88b7
Camilo López <camiloj.lopez79@gmail.com>
2025-05-20 14:36:27 -0400

    Listado de productos y detalle

--- app/(stack)/products/index.tsx
+++ app/(stack)/products/index.tsx
@@ -1,14 +1,33 @@
 // React Native
 import { 
   View,
-  Text
+  Text,
+  FlatList
 } from 'react-native';
+import { Link } from 'expo-router';
+import { products } from '@/store/products.store';
 
 
 const ProductsScreen = () => {
   return (
-    <View>
-      <Text>ProductsScreen</Text>
+    <View className='flex flex-1 px-4'>
+      <FlatList
+        data={ products }
+        keyExtractor={(item) => item.id}
+        renderItem={ ({ item }) => (
+        <View className='mt-10'>
+          <Text className='text-2xl font-work-black'>{ item.title }</Text>
+          <Text className=''>{ item.description }</Text>
+
+          <View className='flex flex-row justify-between mt-2'>
+            <Text className='font-work-black'>{ item.price }</Text>
+            <Link href={ `/(stack)/products/${ item.id }` } className='text-primary'>
+              Details
+            </Link>
+          </View>
+        </View>
+        )}
+      />
     </View>
   );
 }
