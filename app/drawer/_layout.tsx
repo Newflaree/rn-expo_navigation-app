633705956569770b29e0636485e4446bd6d7b2ce
Camilo López <camiloj.lopez79@gmail.com>
2025-05-21 01:41:12 -0400

    Personalizar el Drawer

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -1,19 +1,28 @@
-// React Native
-import { 
-  View,
-  Text
-} from 'react-native';
 import { Drawer } from 'expo-router/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
 
 const DrawerLayout = () => {
   return (
-    <Drawer>
+    <Drawer
+      screenOptions={{
+        overlayColor: 'rgba(0,0,0,0.4)',
+        drawerActiveTintColor: 'indigo',
+        headerShadowVisible: false,
+      }}
+    >
       <Drawer.Screen
         name="user/index"
         options={{
           drawerLabel: 'User',
           title: 'Usuario',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons
+              name='person-circle-outline'
+              color={ color }
+              size={ size }
+            />
+          )
         }}
       />
       <Drawer.Screen
@@ -21,6 +30,13 @@ const DrawerLayout = () => {
         options={{
           drawerLabel: 'Horario',
           title: 'Horario',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons
+              name='calendar-outline'
+              color={ color }
+              size={ size }
+            />
+          )
         }}
       />
     </Drawer>
